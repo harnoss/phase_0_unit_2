9072f49bb2e20405e2cf65bd7ea7e08bcc10efca
Justin Harnoss <justin.harnoss@gmail.com>
2014-05-12 18:05:26 -0700

    1) Ruby Math challenge 2 my solution

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -5,21 +5,50 @@
 # is commented in the file.  
 
 # I worked on this challenge [by myself, with: ].
+# Myself
 
 # 1. Pseudocode
 
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
-
+# INPUT: An array of numbers
+# OUTPUT: A letter grade
+# STEPS TO SOLVE: 
+# Step 1: Calculate average point grade from array of grades
+# Step 2: Translate average point grade into letter grade
 
 # 2. Initial Solution
 
-
-
+def get_grade(array)
+	average_grade = array.inject(:+)/array.length
+	case average_grade
+		when 90..100
+			"A"
+		when 80..90
+			"B"
+		when 70..80
+			"C"
+		when 60..70
+			"D"
+		else "F"
+	end
+end
 
 # 3. Refactored Solution
 
-
+def get_grade(array)
+	case array.inject(:+)/array.length
+		when 90..100
+			"A"
+		when 80..90
+			"B"
+		when 70..80
+			"C"
+		when 60..70
+			"D"
+		else "F"
+	end
+end
 
 # 4. Reflection 
+# This exercice was super helpful to solify my knowledge on hpw to apply the "case" flow controls
+# Usually, I am working with what, ifs but I find the case structure much more intuitive. I was not exactely
+# sure how to do it in detail so I looked up the concept in the Ruby labs on Socrates.
