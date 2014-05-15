08d165a76ccab2ea05bbcf3d486b2b5a235dab63
Justin Harnoss <justin.harnoss@gmail.com>
2014-05-14 18:21:54 -0700

    pseudocode and first solution

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -4,7 +4,7 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself
 
 # 1. Pseudocode
 
@@ -12,10 +12,38 @@
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
 
+# Create a class for Array
+# Create a method that takes an integer and an integer or string ("pad value") as argument
+# gap = array length - integer
+# i = array length
+# until i = integer
+# add an additional element to the array at the last position equal to the pad value
+# if integer <= array length just return the array
 
 # 2. Initial Solution
 
+class Array
 
+def pad!(integer, pad_value = nil)
+	i = self.length
+	until i == integer
+		self.push(pad_value)
+		i += 1
+	end
+	return self
+end
+
+def pad(integer, pad_value = nil)
+	new_array = self
+	i = new_array.length
+	until i == integer
+		new_array.push(pad_value)
+		i += 1
+	end
+	return new_array
+end
+
+end
 
 
 # 3. Refactored Solution
