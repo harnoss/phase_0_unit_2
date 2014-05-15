4040846e470a72d0fdb7b9a9081f53f574bd3726
Justin Harnoss <justin.harnoss@gmail.com>
2014-05-14 18:31:06 -0700

    added refactoring and reflection

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -45,9 +45,31 @@ end
 
 end
 
+#Driver Code 
+puts [1,2,3].pad(5) == [1,2,3,nil,nil]
+puts [1,2,3].pad(5, 'apple') == [1,2,3,'apple', 'apple']
+puts [1,2,3].pad(0) == [1,2,3]
 
 # 3. Refactored Solution
+# Maybe like this (see below) - but i'm not sure if that's much better
 
+class Array
 
+def pad!(integer, pad_value = nil)
+	if integer > self.length
+		(integer-self.length).times self.push(pad_value)
+		return self
+	else 
+		return self
+	end
+end
+
+end
 
 # 4. Reflection 
+# I very much liked this exercise to get into creating classes
+# I got to the pseud code and initial solution pretty quickly
+# but then got hung up on a small thing: I forgot to include a
+# return statement so it always resulted in "nil". This confused
+# me so much that it took me half an hour to figure oút whats wrong.
+# Now I know, whats wrong, when there is just "nil"...
